823d89f16eea78bd4514541405605ef0a2fd181e
Naman Goyal <namangoyal4900@gmail.com>
2022-06-08 03:27:39 +0530

    env in config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,14 @@
 const CONFIG = {
-  GRAPHQL_ENDPOINT: "http://localhost:42000/v1/graphql",
+  GRAPHQL_ENDPOINT:
+    process.env.GRAPHQL_ENDPOINT || "http://localhost:42000/v1/graphql",
 
   CONTRACTS: {
-    CRYPTO_CREDIT: "KT1CWyZrnJaabUbJLoab3MUj4Uj6i48jDG1A",
+    CRYPTO_CREDIT:
+      process.env.CRPTO_CREDIT_CONTRACT ||
+      "KT1CWyZrnJaabUbJLoab3MUj4Uj6i48jDG1A",
   },
 
-  RPC_NODE_URL: "https://ithacanet.smartpy.io/",
+  RPC_NODE_URL: process.env.RPC_NODE_URL || "https://ithacanet.smartpy.io/",
 }
 
 export default CONFIG
