b05f8339c68bc40ada8ec1b0d72c5a7518b307f2
Naman Goyal <namangoyal4900@gmail.com>
2022-06-08 05:54:53 +0530

    fix env variables use

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,14 +1,16 @@
 const CONFIG = {
   GRAPHQL_ENDPOINT:
-    process.env.GRAPHQL_ENDPOINT || "http://localhost:42000/v1/graphql",
+    process.env.REACT_APP_GRAPHQL_ENDPOINT ||
+    "http://localhost:42000/v1/graphql",
 
   CONTRACTS: {
     CRYPTO_CREDIT:
-      process.env.CRPTO_CREDIT_CONTRACT ||
+      process.env.REACT_APP_CRPTO_CREDIT_CONTRACT ||
       "KT1CWyZrnJaabUbJLoab3MUj4Uj6i48jDG1A",
   },
 
-  RPC_NODE_URL: process.env.RPC_NODE_URL || "https://ithacanet.smartpy.io/",
+  RPC_NODE_URL:
+    process.env.REACT_APP_RPC_NODE_URL || "https://ithacanet.smartpy.io/",
 }
 
 export default CONFIG
